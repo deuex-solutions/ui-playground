8b398dd06ab5512db061a0d3796786fd803052ea
Debarshi Bhattacharjee <devdbhatt0@gmail.com>
2022-08-22 12:17:34 +0530

    refactor: added id to button, minor change in styles

--- debarshi/js/components/todoButton.js
+++ debarshi/js/components/todoButton.js
@@ -1,7 +1,12 @@
 const createTodoButton = (props = {}) => {
-  const { textContent = "" } = props
+  const { textContent = "", innerHTML = "" } = props
+
   const todoBtnEl = document.createElement("button")
   todoBtnEl.textContent = textContent
+
+  if (innerHTML) {
+    todoBtnEl.innerHTML = innerHTML
+  }
 
   return todoBtnEl
 }
