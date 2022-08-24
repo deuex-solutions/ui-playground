08b2e91c71e2b64797648296566ce6aee004bbbd
Debarshi Bhattacharjee <devdbhatt0@gmail.com>
2022-08-24 12:19:30 +0530

    feat: added modal component, updated UI styles

--- tic-tac-toe/src/pages/App.tsx
+++ tic-tac-toe/src/pages/App.tsx
@@ -1,14 +1,35 @@
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-        Learn React
-      </a>
-    </header>
-  </div>
-)
+import React, { useState } from 'react'
+import { Board, Modal } from 'components'
 
+const App: React.FC = () => {
+  const [isModalOpen, setIsModalOpen] = useState(true)
+  const [username, setUsername] = useState('')
+  const [choice, setChoice] = useState('')
+
+  const handleModalClose = (open = false) => {
+    setIsModalOpen(open)
+  }
+
+  const handleOnStart = (name: string) => {
+    setUsername(name)
+  }
+
+  const handleChoice = (selectedChoice: string) => {
+    setChoice(selectedChoice)
+  }
+
+  return (
+    <div className="container">
+      <h4>Tic Tac Toe</h4>
+      <Modal
+        isOpen={isModalOpen}
+        onClose={handleModalClose}
+        onStart={handleOnStart}
+        onChoiceChange={handleChoice}
+        selectedChoice={choice}
+      />
+      <Board player={choice} />
+    </div>
+  )
+}
 export default App
