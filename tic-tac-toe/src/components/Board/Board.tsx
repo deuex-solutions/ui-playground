6e7af1ee0053ab5a860057ef2ede285102699885
Debarshi Bhattacharjee <devdbhatt0@gmail.com>
2022-08-24 17:06:13 +0530

    refactor: updated board component to handle game logic

--- tic-tac-toe/src/components/Board/Board.tsx
+++ tic-tac-toe/src/components/Board/Board.tsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react'
-import { Card } from 'components'
+import React, { useCallback, useEffect, useState } from 'react'
+import Card from '../Card/Card'
 import './Board.css'
 
 interface Props {
-  player: string
+  selectedChoice: string
+  onStatusChange: (message: string) => void
+  onReset: () => void
 }
 
 const checkWinner = (cards: string[]) => {
@@ -28,29 +30,60 @@ const checkWinner = (cards: string[]) => {
   return null
 }
 
-const Board: React.FC<Props> = ({ player }) => {
-  const [cards, setCards] = useState<string[]>(new Array(9).fill(''))
-  const [isX, setIsX] = useState(true)
+const getEmptyCards = () => new Array<string>(9).fill('')
+
+const Board: React.FC<Props> = ({ selectedChoice, onStatusChange, onReset }) => {
+  const [cards, setCards] = useState<string[]>(getEmptyCards)
+  const [isX, setIsX] = useState<boolean>(false)
+  const [moves, setMoves] = useState(0)
+
+  const handleReset = useCallback(() => {
+    setCards([...getEmptyCards()])
+    setMoves(0)
+    onReset()
+  }, [onReset])
+
+  useEffect(() => {
+    if (selectedChoice === 'X') {
+      setIsX(true)
+    } else {
+      setIsX(false)
+    }
+  }, [selectedChoice])
+
+  useEffect(() => {
+    const winner = checkWinner(cards)
+    if (winner) {
+      onStatusChange(`Winner is ${winner}`)
+    } else if (moves === cards.length && !winner) {
+      onStatusChange('Match Draw')
+    } else {
+      const playerTurn = isX ? 'X' : '0'
+      onStatusChange(`Player ${playerTurn}'s turn`)
+    }
+  }, [cards, handleReset, isX, moves, onStatusChange])
 
   const handleOnClick = (idx: number) => {
     if (cards[idx] || checkWinner(cards)) return
 
     const duplicateCards: string[] = [...cards]
-    duplicateCards[idx] = isX ? 'X' : '0'
+    duplicateCards[idx] = !isX ? '0' : 'X'
 
     setCards([...duplicateCards])
     setIsX(!isX)
-  }
-
-  if (checkWinner(cards)) {
-    return null
+    setMoves(moves + 1)
   }
 
   return (
-    <div className="board-grid">
-      {new Array(9).fill('').map((_, idx) => (
-        <Card value={cards[idx]} idx={idx} onClick={handleOnClick} />
-      ))}
+    <div className="board-container">
+      <button type="button" className="text-button" onClick={handleReset}>
+        Restart
+      </button>
+      <div className="board-grid">
+        {getEmptyCards().map((_, idx) => (
+          <Card key={idx} value={cards[idx]} idx={idx} onClick={handleOnClick} />
+        ))}
+      </div>
     </div>
   )
 }
