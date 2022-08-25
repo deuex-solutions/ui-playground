a229dbb83a2575425a65ef2123a2ec4e23fd4028
Harsh Vador <harsh.vador@somaiya.edu>
2022-08-25 15:08:34 +0530

    Created Quiz Game

---
+++ harsh-vador/quiz-app-master/backend/src/Routes/Quizzes.js
@@ -0,0 +1,101 @@
+const express = require("express");
+const Router = express.Router();
+const DB = require("./DB");
+const ObjectId = require("mongodb").ObjectId;
+
+// Get Quiz Data
+Router.post("/join", (req, res) => {
+  const { quizId, uid } = req.body;
+  if (!quizId || !uid)
+    return res.status(500).json({ error: "Incomplete Parameters" });
+
+  DB.withDB(async (db) => {
+    try {
+      const cursor = db
+        .collection("quizzes")
+        .find({ _id: new ObjectId(quizId) })
+        .project({
+          // Excluded Fields
+          responses: 0,
+          "questions.options.isCorrect": 0,
+        });
+
+      const quizData = await cursor.toArray();
+      if (!quizData[0].isOpen)
+        res.status(500).json({ error: "ERR:QUIZ_ACCESS_DENIED" });
+      else {
+        const cursor2 = db.collection("users").find({
+          $and: [{ uid }, { attemptedQuiz: ObjectId(quizId) }],
+        });
+
+        const quiz2 = await cursor2.toArray();
+        if (quiz2[0]) {
+          console.log("quiz already attempted");
+          res.status(200).json({
+            error: "ERR:QUIZ_ALREADY_ATTEMPTED",
+          });
+        } else res.status(200).json(quizData[0]);
+      }
+    } catch (error) {
+      res.status(500).json({ error: "ERR:QUIZ_NOT_FOUND" });
+    }
+  }, res);
+});
+
+// Submit the quiz
+Router.post("/submit", (req, res) => {
+  const quiz = req.body;
+  if (!quiz) return res.status(500).json({ error: "Incomplete Parameters" });
+  DB.submitQuiz(quiz, res);
+});
+
+// Create Quiz
+Router.post("/create", (req, res) => {
+  const quiz = req.body;
+  if (!quiz) return res.status(500).json({ error: "Incomplete Parameters" });
+
+  quiz.questions.forEach((question, i) => {
+    question["id"] = i + 1;
+  });
+  DB.createQuiz(quiz, res);
+});
+
+Router.post("/edit", (req, res) => {
+  const { quizId, uid, title, questions, isOpen } = req.body;
+
+  DB.withDB(async (db) => {
+    try {
+      await db.collection("quizzes").updateOne(
+        {
+          $and: [{ uid }, { _id: ObjectId(quizId) }],
+        },
+        {
+          $set: {
+            title,
+            questions,
+            isOpen,
+          },
+        },
+        (err, result) => {
+          if (err) throw err;
+          res.status(200).json({
+            message: "Quiz Updated Successfully.",
+          });
+        }
+      );
+    } catch (error) {
+      res.status(500).json({ error });
+    }
+  });
+});
+
+Router.post("/responses", (req, res) => {
+  const reqBody = req.body;
+  DB.getResponses(reqBody, res);
+});
+
+Router.post("/admin/responses", (req, res) => {
+  const reqBody = req.body;
+  DB.getQuizDetails(reqBody, res);
+});
+module.exports = Router;
---
+++ harsh-vador/quiz-app-master/src/screens/AdminDashBoard/AdminDashBoard.js
@@ -0,0 +1,106 @@
+import React, { useState, useEffect } from "react";
+import "./AdminDashBoard.css";
+import CreatedQuizCard from "../../components/QuizCard/CreatedQuizCard";
+import JoinedQuizCard from "../../components/QuizCard/JoinedQuizCard";
+import LoadingScreen from "../LoadingScreen/LoadingScreen";
+import CreateQuiz from "../CreateQuiz/CreateQuiz";
+
+const AdminDashBoard = ({ user }) => {
+  const [createdQuizzes, setCreatedQuizzes] = useState([]);
+  const [attemptedQuizzes, setAttemptedQuizzes] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [editQuiz, setEditQuiz] = useState([]);
+  // Fetch Data from the API
+  useEffect(() => {
+    if (!user.uid) {
+      setLoading(false);
+      return;
+    }
+    const fetchQuizData = async () => {
+      const results = await fetch(`/API/users/${user.uid}`);
+      const quizData = await results.json();
+      if (quizData.createdQuiz) setCreatedQuizzes(quizData.createdQuiz);
+      if (quizData.attemptedQuiz) setAttemptedQuizzes(quizData.attemptedQuiz);
+      setLoading(false);
+    };
+    if (user) fetchQuizData();
+  }, [user]);
+
+  const editQuizHandle = async (title, questions, isOpen) => {
+    if (!title) setEditQuiz([]);
+    else {
+      setLoading(true);
+      console.dir({
+        quizId: createdQuizzes[editQuiz]._id,
+        uid: user.uid,
+        title,
+        questions,
+        isOpen,
+      });
+      const results = await fetch("/API/quizzes/edit", {
+        method: "POST",
+        body: JSON.stringify({
+          quizId: createdQuizzes[editQuiz]._id,
+          uid: user.uid,
+          title,
+          questions,
+          isOpen,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const submitData = await results.json();
+      console.dir(submitData);
+      const temp = [...createdQuizzes];
+      temp[editQuiz[0]].title = title;
+      temp[editQuiz[0]].questions = questions;
+      temp[editQuiz[0]].isOpen = isOpen;
+      setCreatedQuizzes(temp);
+      setEditQuiz([]);
+      setLoading(false);
+    }
+  };
+
+  if (loading) return <LoadingScreen />;
+
+  if (editQuiz.length)
+    return (
+      <CreateQuiz
+        user={user}
+        quizTitle={createdQuizzes[0].title}
+        questions={createdQuizzes[0].questions}
+        isOpen={createdQuizzes[0].isOpen}
+        editQuizHandle={editQuizHandle}
+      />
+    );
+  return (
+    <div className="dash-body">
+      <div className="quizzes">
+        <div className="heading">
+          <div className="line-left" />
+          <h2>All Quiz </h2>
+          <div className="line" />
+        </div>
+        <div className="card-holder">
+          {createdQuizzes.map((quiz, key) => (
+            <>
+              <CreatedQuizCard
+                key={key}
+                index={key}
+                setEditQuiz={setEditQuiz}
+                title={quiz.title}
+                code={quiz._id}
+                responses={quiz.responses}
+                questions={quiz.questions?.length}
+                isOpen={quiz.isOpen}
+              />
+            </>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AdminDashBoard;
