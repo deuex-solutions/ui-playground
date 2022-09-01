1beb82f05f832562e2297a445cb976c9a046439b
Debarshi Bhattacharjee <devdbhatt0@gmail.com>
2022-09-01 11:10:30 +0530

    refactor(ui): updated foldercard styles to appear as folder icon

--- folder-tree/src/components/folder-card/folder-card.styles.ts
+++ folder-tree/src/components/folder-card/folder-card.styles.ts
@@ -3,13 +3,27 @@ import { makeStyles } from "@material-ui/core";
 export const useFolderCardStyles = makeStyles((theme) => ({
     folderCardContainer: {
         height: "12rem",
+        marginTop: "10px",
     },
     paper: {
         padding: theme.spacing(2),
         textAlign: "center",
         color: theme.palette.text.secondary,
         height: "100%",
-        background: "pink",
+        backgroundColor: "pink",
+        position: "relative",
+        borderRadius: "0 6px 6px 6px",
+        boxShadow: "4px 4px 7px rgba(0, 0, 0, 0.59)",
+        "&::before": {
+            content: "''",
+            width: "50%",
+            height: "12px",
+            borderRadius: "0 20px 0 0",
+            backgroundColor: "pink",
+            position: "absolute",
+            top: "-12px",
+            left: "0",
+        },
     },
 
     file: {
