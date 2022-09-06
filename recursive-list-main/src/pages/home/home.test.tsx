7c380649b50372d60a52541212c4557012690545
Harsh Vador <harsh.vador@somaiya.edu>
2022-09-06 16:24:58 +0530

    (test): test for homepage

---
+++ recursive-list-main/src/pages/home/home.test.tsx
@@ -0,0 +1,32 @@
+import { render, screen } from "@testing-library/react";
+import { ListData } from "components";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./home.component";
+
+describe("Home Page", () => {
+  it("Home Page is rendered properly", async () => {
+    render(
+      <MemoryRouter>
+        <HomePage />
+      </MemoryRouter>
+    );
+
+    expect(screen.getByText("Home")).toHaveAttribute("href", "/");
+  });
+});
+
+it("List data component is rendered properly", async () => {
+  render(
+    <MemoryRouter initialEntries={["/"]}>
+      <Routes>
+        <Route
+          element={
+            <ListData list={[{ name: "index.html", isFolder: false }]} />
+          }
+          path="/*"
+        />
+      </Routes>
+    </MemoryRouter>
+  );
+  expect(screen.getByText("index.html")).toBeInTheDocument();
+});
